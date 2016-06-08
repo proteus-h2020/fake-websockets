a85747a040047361f4bd0e25fd585858f4c31f42
0xNacho <nachogarcia91@gmail.com>
2016-06-08 17:23:13 +0200

    Add swimlane

---
+++ models/swimlane.js
@@ -0,0 +1,39 @@
+var moment = require('moment');
+
+function swimlaneGenerator() {
+  this.item = 0;
+  this.lanes = [];
+
+  this.lanes.push({ id: 0, label: 'Carril 0' });
+  this.lanes.push({ id: 1, label: 'Carril 1' });
+  this.lanes.push({ id: 2, label: 'Carril 2' });
+
+
+  this.date = moment(new Date(2009, 9, 18, 0, 0, 0));
+
+  this.next = () => {
+    if (this.item > 1000000) {
+      this.item = 0;
+      this.date = moment(new Date(2009, 9, 18, 0, 0, 0));
+    }
+
+    var data = { lanes: this.lanes, items: [] };
+
+    for (var i = 0; i < this.lanes.length; i++) {
+      data.items.push({
+        id: this.item,
+        lane: i,
+        start: this.date.add('days', 1).toString(),
+        end: this.date.add('days', 3).toString(),
+        class: 'past'
+      });
+
+      this.date.add('days', 2);
+      this.item++;
+    }
+    return JSON.stringify(data);
+
+  }
+}
+
+module.exports = swimlaneGenerator;
