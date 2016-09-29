4d3f3654df636bc2bff64b1dae2cb680f16489dd
0xNacho <nachogarcia91@gmail.com>
2016-09-29 12:26:05 +0200

    Change linechart format

--- models/linechart.js
+++ models/linechart.js
@@ -1,20 +1,14 @@
 function linechartGenerator() {
   this.x = 0;
 
-  this.next =  () => {
+  this.next = () => {
     var series1 = [];
     if (this.x > 1000000) {
       this.x = 0;
     }
-    for (var i = 0; i < 3; i++) {
-      series1.push({
-        x: (++this.x),
-        y: parseInt(Math.random() * (100 - 20) + 20)
-      });
-    }
-    var data = [{ key: "series1", values: series1 }];
-    return JSON.stringify(data);
   }
+  var data = [{ key: "series1", x: (++this.x), y: parseInt(Math.random() * (100 - 20) + 20) }];
+  return JSON.stringify(data);
 }
 
 module.exports = linechartGenerator;
