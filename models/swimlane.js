8da3ee0d72b84968ac4d907e4febbed860ee6357
0xNacho <nachogarcia91@gmail.com>
2016-09-30 09:43:40 +0200

    Swimlane

--- models/swimlane.js
+++ models/swimlane.js
@@ -16,21 +16,34 @@ function swimlaneGenerator() {
       this.item = 0;
       this.date = moment(new Date(2009, 9, 18, 0, 0, 0));
     }
+    var data = [];
 
-    var data = { lanes: this.lanes, items: [] };
-
-    for (var i = 0; i < this.lanes.length; i++) {
-      data.items.push({
-        id: this.item,
-        lane: i,
-        start: this.date.add('days', 1).toString(),
-        end: this.date.add('days', 3).toString(),
-        class: 'past'
-      });
-
-      this.date.add('days', 2);
-      this.item++;
+    for (let i = 0; i < this.lanes.length; i++) {
+      let lane = this.lanes[i];
+      data.push({
+        key: lane.label,
+        x: this.date.add('days', 1).toString(),
+        y: this.date.add('days', 3).toString(),
+      })
     }
+    
+
+    /**
+        var data = { lanes: this.lanes, items: [] };
+    
+        for (var i = 0; i < this.lanes.length; i++) {
+          data.items.push({
+            id: this.item,
+            lane: i,
+            start: this.date.add('days', 1).toString(),
+            end: this.date.add('days', 3).toString(),
+            class: 'past'
+          });
+    
+          this.date.add('days', 2);
+          this.item++;
+        }
+      **/
     return JSON.stringify(data);
 
   }
